a1e71a99f97a19d1894c68116785d6e662f37f5c
BernardAli <alibernard.coding@gmail.com>
2022-10-27 17:15:30 +0000

    Templates Added

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ben Ali',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'Oct 27, 2022'
+    },
+    {
+        'author': 'Muna Ali',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Oct 26, 2022'
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title="About Page")
 
 
 if __name__ == '__main__':
